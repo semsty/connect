b0deaf54db9efebd25f43ed2b693f585094b7c4d
sk <semsty@gmail.com>
2020-10-28 13:35:24 +0300

    tmp

--- tests/app/docker/entrypoint.sh
+++ tests/app/docker/entrypoint.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 export CONNECT_CORE_DIR="/app"
-composer install --prefer-dist --no-interaction \
-&& tests/app/docker/wait-for-it.sh $POSTGRES_HOST:$POSTGRES_PORT \
+tests/app/docker/wait-for-it.sh $POSTGRES_HOST:$POSTGRES_PORT \
 && php tests/yii pgsql-migrate/up --interactive=0 \
 && exec "$@"
